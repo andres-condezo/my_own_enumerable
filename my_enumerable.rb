85f2e4c0266ae80aad8d9b1ee6b6f97283444896
andres-condezo <ra.condezomonge@gmail.com>
2022-05-06 12:32:06 -0500

    Create any and filter methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -13,4 +13,25 @@ module MyEnumerable
     end
     true
   end
+
+  def any?
+    if block_given?
+      each { |n| return true if yield(n) }
+    elsif param.nil?
+      each { |n| return true if n }
+    elsif !param.nil? && (param.is_a? Class)
+      each { |n| return true if [n.class, n.class.superclass, n.class.superclass].include?(param) }
+    elsif !param.nil? && (param.is_a? Regexp)
+      each { |n| return true if n.match(param) }
+    else
+      each { |n| return true if n == param }
+    end
+    false
+  end
+
+  def filter
+    list = []
+    each { |item| list.push(item) if yield(item) }
+    list
+  end
 end
