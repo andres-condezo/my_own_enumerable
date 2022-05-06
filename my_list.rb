62ec1fd217849eaf11ba325e191cbf5373e9a31a
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-05-06 14:44:54 -0300

    Join Module with MyList class, Add tests

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,8 @@
+require_relative 'my_enumerable'
+
 class MyList
+  include MyEnumerable
+
   def initialize(*list)
     @list = list
   end
@@ -11,3 +15,22 @@ class MyList
     end
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+# => #<MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+list.all? { |e| e < 5 }
+# => true
+list.all? { |e| e > 5 }
+# => false
+
+# Test #any?
+list.any? { |e| e == 2 }
+# => true
+list.any? { |e| e == 5 }
+# => false
+
+# Test #filter
+list.filter(&:even?)
+# => [2, 4]
